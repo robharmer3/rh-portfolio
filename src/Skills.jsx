2fac3c5fe7014c3058ede0676ebdaaf3a6946ceb
robharmer3 <rob.harmer3@gmail.com>
2025-07-03 21:39:35 +0100

    skills page CSS updated

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Error from "./Common/Error";
 import Loading from "./Common/Loading";
 import { getSkills } from "./endpoints";
@@ -6,7 +7,6 @@ import useFetchApi from "./endpoints Hook";
 export default function Skills() {
   const { isLoading, isError, data } = useFetchApi(getSkills);
   const { skills } = data;
-  console.log(skills);
 
   if (isLoading) {
     return <Loading />;
@@ -20,70 +20,104 @@ export default function Skills() {
     return (
       <>
         <h2>Skills</h2>
-        <h3>Languages</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 1) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Testing</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 2) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Back-End</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 3) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Front-End</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 4) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Hosting</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 5) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Techniques</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 6) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Engineering</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 7) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
-        <h3>Personal</h3>
-        <ul>
-          {skills.map((skill) => {
-            if (skill.category === 8) {
-              return <li key={skill.skill_id}>{skill.title}</li>;
-            }
-          })}
-        </ul>
+        <div style={{ width: "100%", height: "100%", position: "relative" }}>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Languages</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 1) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Testing</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 2) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Back-End</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 3) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Front-End</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 4) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Hosting</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 5) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Techniques</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 6) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Engineering</h3>
+              <ul>
+                {skills.map((skill) => {
+                  if (skill.category === 7) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+          <div style={{ width: "100%", height: "100%", display: "flex" }}>
+            <div className="skills">
+              <h3>Personal</h3>
+              <ul className="skills">
+                {skills.map((skill) => {
+                  if (skill.category === 8) {
+                    return <li key={skill.skill_id}>{skill.title}</li>;
+                  }
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
