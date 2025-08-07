40d966dd89bdb69906aa6bab20be0009fdc9c0db
robharmer3 <rob.harmer3@gmail.com>
2025-08-07 21:17:11 +0100

    start of added screenshots to projects

--- src/Project/ProjectCard.jsx
+++ src/Project/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 
 function ProjectCard({ project, categories, index }) {
+  console.log(typeof project.avatar_url);
   return (
     <div className="project" style={{ translate: `${-100 * index}%` }}>
       <h3>
@@ -8,8 +9,12 @@ function ProjectCard({ project, categories, index }) {
       </h3>
       <p>Project Overview: {project.description}</p>
       <p>Project Category: {categories[project.category - 1].title}</p>
-      {/* <p>Tech Stack: </p> */}
+      <p>Tech Stack: </p>
       <p>{project.link}</p>
+      <img
+        src="https://en.wikipedia.org/wiki/Tiger#/media/File:Walking_tiger_female.jpg"
+        alt=""
+      />
     </div>
   );
 }
