24422e894c8747057ebec4c76aa6b6b143cfc7b1
robharmer3 <rob.harmer3@gmail.com>
2025-09-11 21:58:09 +0100

    profile pic added to home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,18 @@
-import CVButton from "./Features/CV-Button";
+import profilePic from "./assets/IMG-20241022-WA0002 (1).jpg";
 
 function Home() {
   return (
     <>
       {/* <img src="" alt="" /> */}
-      <h2>Hi there 👋</h2>
-      <h3>Welcome to my Software Portfolio</h3>
-      <p>
-        My name is Rob, and I may looking to learn new skills in the world of
-        coding!
-      </p>
-      {/* <CVButton /> */}
+      <img src={profilePic} alt="Me!" id="profile-pic" />
+      <div id="home-page">
+        <h2>Hi there 👋</h2>
+        <h3>Welcome to my Software Portfolio</h3>
+        <p>
+          My name is Rob, and I may looking to learn new skills in the world of
+          coding!
+        </p>
+      </div>
     </>
   );
 }
